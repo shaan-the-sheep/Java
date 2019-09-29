110510cf408a85a56734b4b161110a453f6d72c2
Shaan <shaanjassal101@gmail.com>
2019-09-29 14:18:52 +0100

    Asks user to enter random characters from word
    Signed-off-by: Shaan <shaanjassal101@gmail.com>

--- School/PIN/src/pin/PIN.java
+++ School/PIN/src/pin/PIN.java
@@ -1,11 +1,14 @@
 package pin;
 import java.util.Scanner;
+import java.util.Random;
+import java.util.ArrayList;
 
 public class PIN {
 
     public static void main(String[] args) {
-        int pin = 1234;
-        String word = "hello";
+        final int pin = 1234;
+        final String word = "hello";
+        
         Scanner myObj = new Scanner(System.in);  
         int n = 0;
         while (n < 3){
@@ -15,23 +18,37 @@ public class PIN {
                 System.out.println("You are logged in");
                 break;
             } else {
-                System.out.println("Wrong. Try again");
+                System.out.println("Wrong.");
                 n++;
             }
         }
-        if (n == 3){
+        if (n == 3){    // Wrong attempts. Account is locked
+            int s = 0;
             Scanner myObj2 = new Scanner(System.in);
+            Random rand = new Random();
+            ArrayList<Integer> nums = new ArrayList<Integer>();
             for (int i = 0; i < 3; i = i + 1) {
-                System.out.println("Your account is locked. Enter three characters from memorable word: ");
-                String l = myObj2.nextLine();
-                if (word.contains(l)){
-                    System.out.println("Pin: " + pin);
-                    break;
+                int int1 = rand.nextInt(word.length());
+                if (nums.contains(int1)) {
+                    i = i - 1;
                 }
-                
-                
+                else{
+                    nums.add(int1);
+                }
+            } 
+            for (int i = 0; i < 3; i = i + 1) {
+                System.out.println("Your account is locked. Enter character: "+ (nums.get(i)+1) + " of memorable word");
+                char l = myObj2.next().charAt(0);
+                if (l == (word.charAt(nums.get(i)))){
+                    s++;
+                }
             }
-                
+            if (s==3){
+                System.out.println("Pin: " + pin);
+            }
+            else{
+                System.out.println("One or more characters incorrect. Account still locked.");
+            }
         }
     }
 }
